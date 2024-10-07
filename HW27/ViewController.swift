dec559a4b9fbd0325755cb1b08d041a924964b41
Maksim <i@mgromov.ru>
2024-10-07 18:25:04 +0300

    1

--- HW27/ViewController.swift
+++ HW27/ViewController.swift
@@ -6,12 +6,48 @@
 //
 
 import UIKit
-
+class BankAccount {
+    var balance: Double = 0
+    let lock = NSLock()
+    func deposit(_ amount: Double) {
+        lock.lock()
+        balance += amount
+        lock.unlock()
+    }
+    func withdraw(_ amount: Double) {
+        lock.lock()
+        balance -= amount
+        lock.unlock()
+    }
+}
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let thread = Thread {
+            print("Hello from another thread")
+        }
+        thread.start()
+        
+        let queue = DispatchQueue(label: "com.gm.queue", attributes: .concurrent)
+        var acc1 = BankAccount()
+        var acc2 = BankAccount()
+        queue.sync {
+            acc1.deposit(100)
+            acc1.withdraw(50)
+            acc1.withdraw(25)
+            acc2.deposit(10)
+            acc1.deposit(5)
+            acc2.deposit(20)
+            acc2.withdraw(5)
+        }
+        if acc1.balance != 30{
+            print("Беда1")
+        }
+        if acc2.balance != 25{
+            print("Беда2")
+        }
+        
+        
     }
 
 
